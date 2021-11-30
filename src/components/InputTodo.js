e02a305a094596094e475089a800f75ac8916703
brytebee <brytebee@gmail.com>
2021-11-30 15:46:51 +0100

    Add more styling for body and header

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class InputTodo extends Component {
   state = {
-    title: "",
+    title: '',
   };
 
   onChange = (e) => {
@@ -16,23 +16,24 @@ class InputTodo extends Component {
     if (this.state.title.trim()) {
       this.props.addTodoProps(this.state.title);
       this.setState({
-        title: "",
+        title: '',
       });
     }
-    alert("Please write item");
+    alert('Please write item');
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
           placeholder="Add Todo..."
+          className="input-text"
           value={this.state.title}
           name="title"
           onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     );
   }
